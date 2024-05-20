9d715d886984945e16094efd08b7e1c70f25d1e0
Mikki Friend <realmikkifriend@gmail.com>
2024-05-20 14:59:30 -0500

    build: config file changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: "./src/js/index.js",
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
         clean: true,
     },
     resolve: {
         alias: {
-            svelte: path.resolve('node_modules', 'svelte/src/runtime')
+            svelte: path.resolve("node_modules", "svelte/src/runtime"),
         },
-        extensions: ['.mjs', '.js', '.svelte'],
-        mainFields: ['svelte', 'browser', 'module', 'main'],
-        conditionNames: ['svelte', 'browser', 'import']
+        extensions: [".mjs", ".js", ".svelte"],
+        mainFields: ["svelte", "browser", "module", "main"],
+        conditionNames: ["svelte", "browser", "import"],
     },
     module: {
         rules: [
             {
                 test: /\.svelte$/,
-                use: 'svelte-loader'
+                use: "svelte-loader",
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader'],
+                use: ["style-loader", "css-loader", "postcss-loader"],
             },
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/html/index.html',
-            filename: 'index.html',
+            template: "./src/html/index.html",
+            filename: "index.html",
         }),
-        new Dotenv()
+        new Dotenv(),
     ],
+    infrastructureLogging: {
+        level: "warn",
+    },
     devServer: {
         hot: false,
-        liveReload: true
+        liveReload: true,
     },
 };
