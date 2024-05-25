823e0068aab550c907c472208f830fcdf255b9fc
Mikki Friend <realmikkifriend@gmail.com>
2024-05-24 20:18:16 -0500

    feat: can mark task done

--- src/js/api.js
+++ src/js/api.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 export async function fetchTodoistData(resourceTypes, syncToken, accessToken) {
     const response = await fetch("https://api.todoist.com/sync/v9/sync", {
         method: "POST",
@@ -8,6 +10,34 @@ export async function fetchTodoistData(resourceTypes, syncToken, accessToken) {
         body: new URLSearchParams({
             sync_token: syncToken,
             resource_types: JSON.stringify(resourceTypes),
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+export async function markTaskDone(taskId, accessToken) {
+    const response = await fetch("https://api.todoist.com/sync/v9/sync", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            commands: JSON.stringify([
+                {
+                    type: "item_complete",
+                    uuid: uuidv4(),
+                    args: {
+                        id: taskId,
+                    },
+                },
+            ]),
         }),
     });
 
--- src/js/taskHandlers.js
+++ src/js/taskHandlers.js
@@ -1,10 +1,22 @@
-import { todoistResources, refreshData } from "./stores";
+import { todoistResources, refreshData, todoistAccessToken, todoistError } from "./stores";
+import { markTaskDone } from "./api";
 
-export const handleTaskDone = (event, setPreviousFirstDueTask, setFirstDueTask) => {
+export const handleTaskDone = async (event, setPreviousFirstDueTask, setFirstDueTask) => {
+    let accessToken;
+    todoistAccessToken.subscribe(($) => {
+        accessToken = $;
+    });
+
+    if (!accessToken) {
+        todoistError.set("No access token found.");
+        return;
+    }
+
     todoistResources.update(($resources) => {
+        console.log($resources.dueTasks);
         const index = $resources.dueTasks.findIndex((task) => task.id === event.detail.task.id);
         if (index !== -1 && index < $resources.dueTasks.length - 1) {
-            setPreviousFirstDueTask($resources.dueTasks[index]);
+            setPreviousFirstDueTask($resources.dueTasks[index + 1]);
             setFirstDueTask($resources.dueTasks[index + 1]);
         } else {
             setPreviousFirstDueTask(null);
@@ -12,5 +24,13 @@ export const handleTaskDone = (event, setPreviousFirstDueTask, setFirstDueTask) 
         }
         return $resources;
     });
+
+    try {
+        await markTaskDone(event.detail.task.id, accessToken);
+    } catch (error) {
+        todoistError.set(`Failed to mark task done: ${error.message}`);
+        return;
+    }
+
     refreshData();
 };
