9f776348554736ff1c11ff055e7814ca6d6106fb
Mikki Friend <realmikkifriend@gmail.com>
2024-06-01 17:16:15 -0500

    feat: show context & remaining next to menu button

--- src/html/Sidebar.svelte
+++ src/html/Sidebar.svelte
@@ -8,13 +8,36 @@
         todoistError,
         syncToken,
         userSettings,
+        firstDueTask,
     } from "../js/stores";
 
     let resources;
+    let dueTasksInContext = 0;
+    let currentContextName = "";
 
     todoistResources.subscribe(($resources) => {
         resources = $resources;
+        filterDueTasksInContext();
     });
+
+    firstDueTask.subscribe(($firstDueTask) => {
+        filterDueTasksInContext();
+    });
+
+    function filterDueTasksInContext() {
+        if (resources && resources.dueTasks && firstDueTask) {
+            let $firstDueTask;
+            firstDueTask.subscribe((value) => {
+                $firstDueTask = value;
+            })();
+            dueTasksInContext = resources.dueTasks.filter(
+                (task) => task.context_id === $firstDueTask.context_id,
+            ).length;
+
+            const context = resources.contexts.find((c) => c.id === $firstDueTask.context_id);
+            currentContextName = context ? context.name : "";
+        }
+    }
 
     function handleLogout() {
         todoistAccessToken.set("");
@@ -27,12 +50,15 @@
 
 <div class="drawer">
     <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-        <label
-            for="my-drawer"
-            class="btn drawer-button absolute mt-0 bg-transparent hover:bg-primary"
-            ><Bars3Icon class="h-8 w-8" /></label
-        >
+    <div class="drawer-content flex flex-row items-center">
+        <label for="my-drawer" class="btn drawer-button mt-0 bg-transparent hover:bg-primary"
+            ><Bars3Icon class="h-8 w-8" />
+        </label>
+        {#if $firstDueTask}
+            <div class="badge badge-outline whitespace-nowrap opacity-75">
+                {dueTasksInContext} left in {currentContextName}
+            </div>
+        {/if}
     </div>
     <div class="drawer-side z-10">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
