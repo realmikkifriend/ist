5a90f5e9b88dd65b4ca51f58861a4d490f91c758
Mikki Friend <realmikkifriend@gmail.com>
2024-06-04 11:00:57 -0500

    fix: extracted buttons, defer dialog updates on task change

--- src/html/defer/TimePicker.svelte
+++ src/html/defer/TimePicker.svelte
@@ -1,7 +1,9 @@
 <script>
-    import { createEventDispatcher, onDestroy } from "svelte";
+    import { createEventDispatcher, onDestroy, onMount, afterUpdate } from "svelte";
     import { DateTime } from "luxon";
     import { createTomorrowDateWithTime } from "../../js/time";
+    import buttonConfigs from "../../js/deferButtons";
+
     export let task;
 
     const dispatch = createEventDispatcher();
@@ -18,40 +20,24 @@
             now = DateTime.now();
         extractedTime = result.extractedTime;
         tomorrowInMS = tomorrow.diff(now).milliseconds;
+
+        buttonConfigs[0].text = `tomorrow ${extractedTime}`;
+        buttonConfigs[0].time = tomorrowInMS;
     };
 
-    updateMilliseconds();
+    onMount(() => {
+        updateMilliseconds();
+    });
+
+    afterUpdate(() => {
+        updateMilliseconds();
+    });
 
     const interval = setInterval(updateMilliseconds, 60000);
 
     onDestroy(() => {
         clearInterval(interval);
     });
-
-    const buttonConfigs = [
-        {
-            text: `tomorrow ${extractedTime}`,
-            time: tomorrowInMS,
-            basis: "basis-full",
-            height: "h-8",
-        },
-        { text: "1 minute", time: 60 * 1000, basis: "basis-[48.5%]", height: "h-5" },
-        { text: "3 minutes", time: 3 * 60 * 1000, basis: "basis-[48.5%]", height: "h-5" },
-        { text: "10 minutes", time: 10 * 60 * 1000, basis: "basis-[48.5%]", height: "h-6" },
-        { text: "15 minutes", time: 15 * 60 * 1000, basis: "basis-[48.5%]", height: "h-6" },
-        { text: "30 minutes", time: 30 * 60 * 1000, basis: "basis-[48.5%]", height: "h-7" },
-        { text: "45 minutes", time: 45 * 60 * 1000, basis: "basis-[48.5%]", height: "h-7" },
-        { text: "1 hour", time: 60 * 60 * 1000, basis: "basis-[48.5%]", height: "h-8" },
-        { text: "90 minutes", time: 90 * 60 * 1000, basis: "basis-[48.5%]", height: "h-8" },
-        { text: "2 hrs", time: 2 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "3 hrs", time: 3 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "4 hrs", time: 4 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "6 hrs", time: 6 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "8 hrs", time: 8 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "12 hrs", time: 12 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "18 hrs", time: 18 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-        { text: "24 hrs", time: 24 * 60 * 60 * 1000, basis: "basis-[22.75%]", height: "h-8" },
-    ];
 </script>
 
 <div class="mt-4 flex w-72 flex-row flex-wrap gap-2">
