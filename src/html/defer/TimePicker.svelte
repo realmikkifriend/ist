677cfd83a0dd8b80bbbd7aaceca75a523aea6716
Mikki Friend <realmikkifriend@gmail.com>
2024-06-10 14:35:14 -0500

    feat: time picker displays calculated future time

--- src/html/defer/TimePicker.svelte
+++ src/html/defer/TimePicker.svelte
@@ -2,7 +2,7 @@
     import { createEventDispatcher, onDestroy, onMount, afterUpdate } from "svelte";
     import { DateTime } from "luxon";
     import { createTomorrowDateWithTime } from "../../js/time";
-    import buttonConfigs from "../../js/deferButtons";
+    import buttons from "../../js/deferButtons";
 
     export let task;
 
@@ -21,8 +21,13 @@
         extractedTime = result.extractedTime;
         tomorrowInMS = tomorrow.diff(now).milliseconds;
 
-        buttonConfigs[0].text = `tomorrow ${extractedTime}`;
-        buttonConfigs[0].time = tomorrowInMS;
+        buttons[0].text = `tomorrow ${extractedTime}`;
+        buttons[0].ms = tomorrowInMS;
+
+        for (let i = 1; i < buttons.length; i++) {
+            let futureTime = now.plus({ milliseconds: buttons[i].ms });
+            buttons[i].time = futureTime.toFormat("h:mm a");
+        }
     };
 
     onMount(() => {
@@ -33,19 +38,24 @@
         updateMilliseconds();
     });
 
-    const interval = setInterval(updateMilliseconds, 60000);
+    const interval = setInterval(updateMilliseconds, 30000);
 
     onDestroy(() => {
         clearInterval(interval);
     });
 </script>
 
-<div class="mt-4 flex w-72 flex-row flex-wrap gap-2">
-    {#each buttonConfigs as { text, time, basis, height }}
-        <button
-            class="btn mb-2 min-h-4 rounded-md bg-neutral px-1 hover:bg-secondary {basis} {height}"
-            on:click={() => handleDefer(time)}
-            >{text}
-        </button>
+<div class="mt-4 flex w-72 flex-row flex-wrap gap-x-2 gap-y-1">
+    {#each buttons as { text, ms, basis, height, time }}
+        <div class={basis}>
+            <button
+                class="btn min-h-4 w-full rounded-md bg-neutral px-1 hover:bg-secondary {height}"
+                on:click={() => handleDefer(ms)}
+                >{text}
+            </button>
+            {#if time}
+                <span class="block text-left text-xs opacity-50">{time}</span>
+            {/if}
+        </div>
     {/each}
 </div>
