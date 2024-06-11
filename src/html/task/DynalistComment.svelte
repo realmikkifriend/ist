a041fb6531db306da96bc7217247cb2d0a8094d6
Mikki Friend <realmikkifriend@gmail.com>
2024-06-11 14:02:42 -0500

    refactor: moved Dynalist functions into separate file

--- src/html/task/DynalistComment.svelte
+++ src/html/task/DynalistComment.svelte
@@ -3,92 +3,35 @@
     import { ArrowPathIcon } from "@krowten/svelte-heroicons";
     import { onMount } from "svelte";
     import DynalistTypeMenu from "./DynalistTypeMenu.svelte";
+    import { fetchDynalistDocument, processNode, generateDynalistComment } from "../../js/dynalist";
+
     export let url, accessToken;
 
     let dynalistContent;
 
     onMount(async () => {
-        const lastIndex = url.lastIndexOf("/"),
-            hashIndex = url.indexOf("#z=", lastIndex),
-            dynalistFileID = url.slice(lastIndex + 1, hashIndex),
-            dynalistSubItem = hashIndex === -1 ? undefined : url.slice(hashIndex + 3);
-
-        let data;
-
         try {
-            const response = await fetch("https://dynalist.io/api/v1/doc/read", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    token: accessToken,
-                    file_id: dynalistFileID,
-                }),
-            });
+            const { data, dynalistSubItem } = await fetchDynalistDocument(url, accessToken);
+            let rootNode, dynalistObject;
 
-            if (!response.ok) {
-                throw new Error(`Error fetching the document: ${response.statusText}`);
+            if (dynalistSubItem) {
+                rootNode = data.nodes.find((node) => node.id === dynalistSubItem);
+            } else {
+                rootNode = data.nodes.find((node) => node.id === "root");
             }
 
-            data = await response.json();
+            if (rootNode) {
+                dynalistObject = processNode(rootNode, data);
+                dynalistContent =
+                    generateDynalistComment(dynalistObject) ||
+                    "Unsupported format, but stay tuned.";
+            } else {
+                console.error("Specified node not in document.");
+            }
         } catch (error) {
-            console.error("Failed to retrieve the Dynalist document:", error);
-            return;
-        }
-
-        let rootNode, dynalistObject;
-
-        if (dynalistSubItem) {
-            rootNode = data.nodes.find((node) => node.id === dynalistSubItem);
-        } else {
-            rootNode = data.nodes.find((node) => node.id === "root");
-        }
-
-        if (rootNode) {
-            dynalistObject = processNode(rootNode, data);
-            dynalistContent =
-                generateDynalistComment(dynalistObject) || "Unsupported format, but stay tuned.";
-        } else {
-            console.error("Specified node not in document.");
+            console.error("Error during Dynalist document retrieval or processing:", error);
         }
     });
-
-    const processNode = (node, data) => {
-        const { created, modified, collapsed, ...filteredNode } = node;
-
-        if (node.children && node.children.length > 0) {
-            filteredNode.children = node.children
-                .map((childId) => {
-                    let child = data.nodes.find((node) => node.id === childId);
-
-                    if (child) {
-                        return processNode(child, data);
-                    }
-
-                    return null;
-                })
-                .filter((child) => child !== null);
-        }
-
-        return filteredNode;
-    };
-
-    const generateDynalistComment = (node, indent = 0) => {
-        if (!node || !Array.isArray(node.children)) return "";
-
-        const processNode = (node, level) => {
-            const markdown = `${"  ".repeat(level)}- ${node.content}\n`;
-            return (
-                node.children?.reduce(
-                    (acc, child) => acc + processNode(child, level + 1),
-                    markdown,
-                ) || markdown
-            );
-        };
-
-        return node.children.reduce((acc, child) => acc + processNode(child, indent), "");
-    };
 </script>
 
 {#if dynalistContent}
