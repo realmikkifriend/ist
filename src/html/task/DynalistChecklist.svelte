4f4538ab362c87aa965dd477feff300d74e07d6d
Mikki Friend <realmikkifriend@gmail.com>
2024-06-11 22:31:19 -0500

    feat: completing checklist item shows next

--- src/html/task/DynalistChecklist.svelte
+++ src/html/task/DynalistChecklist.svelte
@@ -3,12 +3,44 @@
     export let content;
 
     function parseContent(content) {
-        return content.split("\n").map((item) => item.substring(2).trim());
+        const lines = content.split("\n");
+        const result = [];
+
+        for (let line of lines) {
+            if (line.startsWith("  - ")) {
+                if (result.length > 0) {
+                    result[result.length - 1] += "\n" + line;
+                }
+            } else {
+                result.push(line.substring(2).trim());
+            }
+        }
+
+        return result;
     }
 
     const checklistItems = parseContent(content);
+    let currentIndex = 0;
+
+    let buttonElement;
+
+    function showNextItem() {
+        buttonElement.classList.add("bg-secondary");
+
+        setTimeout(() => {
+            if (currentIndex < checklistItems.length - 1) {
+                currentIndex++;
+                buttonElement.classList.remove("bg-secondary");
+            }
+        }, 200);
+    }
 </script>
 
-{#each checklistItems as item}
-    <Markdown md={item} />
-{/each}
+<div>
+    <button
+        bind:this={buttonElement}
+        class="float-left mr-2 mt-1 inline-block h-5 w-5 cursor-pointer border-2 border-secondary transition-colors"
+        on:click={showNextItem}
+    />
+    <Markdown md={checklistItems[currentIndex]} />
+</div>
