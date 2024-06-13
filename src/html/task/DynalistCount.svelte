6d303eb453840322cd2547054a652e84117062da
Mikki Friend <realmikkifriend@gmail.com>
2024-06-13 12:32:43 -0500

    style: improved display on mobile

--- src/html/task/DynalistCount.svelte
+++ src/html/task/DynalistCount.svelte
@@ -32,21 +32,22 @@
     }
 </script>
 
-<span class="flex w-full justify-between">
-    <span>
-        <span class="text-xl">{content.content}</span>
-        <br />
-        <span class="text-2xl">{current}</span>
-        <span class="mx-2 text-lg">&#8725;{total}</span>
-        <small>{`${Math.round((current / total) * 100)}%`}</small>
-    </span>
-
-    <span class="content-end">
-        {#each options as option}
-            <button
-                class="btn ml-1 h-8 min-h-8 bg-primary px-2 pb-1 pt-0 text-white hover:bg-base-100"
-                on:click={() => handleCount(option)}>{option}</button
-            >
-        {/each}
+<span class="flex w-full flex-col justify-between">
+    <span class="w-full text-xl">{content.content}</span>
+    <span class="flex w-full items-end justify-between">
+        <span class="flex flex-nowrap items-baseline">
+            <span class="text-2xl">{current}</span>
+            <span class="mx-1 text-lg">&#8725;{total}</span>
+            <small>{`${Math.round((current / total) * 100)}%`}</small>
+        </span>
+        <span class="flex flex-nowrap items-end">
+            {#each options as option}
+                <button
+                    class="btn ml-1 h-8 min-h-8 bg-primary px-1 pb-1 pt-0 text-white hover:bg-base-100"
+                    on:click={() => handleCount(option)}
+                    >{option}
+                </button>
+            {/each}
+        </span>
     </span>
 </span>
