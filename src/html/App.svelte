05fb26d258b90b5f5483ec8afbaaf5d7708df491
Mikki Friend <realmikkifriend@gmail.com>
2024-06-18 11:56:00 -0500

    refactor: switched loading to Svelte await syntax

--- src/html/App.svelte
+++ src/html/App.svelte
@@ -1,31 +1,32 @@
 <script>
     import { onMount, onDestroy } from "svelte";
     import { DateTime } from "luxon";
+    import { ArrowPathIcon } from "@krowten/svelte-heroicons";
     import { todoistResources, todoistError, userSettings, firstDueTask } from "../js/stores";
     import { checkAndUpdateFirstDueTask } from "../js/first";
     import { refreshData } from "../js/api";
     import { error } from "../js/toasts";
     import { handleTaskDone, handleTaskDefer } from "../js/taskHandlers";
-    import { ArrowPathIcon } from "@krowten/svelte-heroicons";
     import Sidebar from "./sidebar/Sidebar.svelte";
     import Task from "./task/Task.svelte";
 
-    let intervalId,
-        isSpinning = false;
+    let autoRefresh,
+        isSpinning = false,
+        dataPromise;
 
     $: selectedContextId = $userSettings.selectedContextId;
     $: updateFirstDueTask($todoistResources, { selectedContextId });
 
-    onMount(async () => {
-        await handleRefresh();
+    onMount(() => {
+        dataPromise = handleRefresh();
 
-        intervalId = setInterval(async () => {
+        autoRefresh = setInterval(async () => {
             await handleRefresh();
         }, 300000);
     });
 
     onDestroy(() => {
-        clearInterval(intervalId);
+        clearInterval(autoRefresh);
     });
 
     const updateFirstDueTask = ($resources, $settings) => {
@@ -65,19 +66,25 @@
 
 <Sidebar />
 
-{#if $todoistResources.items}
-    {#if $firstDueTask}
-        <Task task={$firstDueTask} on:done={handleDone} on:defer={handleDefer} />
-    {:else}
-        <div class="hero">No due tasks</div>
-    {/if}
-
-    <button class="fixed bottom-2 right-2 opacity-50" on:click={handleRefresh}>
-        <ArrowPathIcon class="h-6 w-6 {isSpinning ? 'animate-spin' : ''}" />
-    </button>
-{:else}
+{#await dataPromise}
     <div class="hero">Loading...</div>
-{/if}
+{:then}
+    {#if $todoistResources.items}
+        {#if $todoistResources.dueTasks}
+            <Task task={$firstDueTask} on:done={handleDone} on:defer={handleDefer} />
+        {:else}
+            <div class="hero">No due tasks</div>
+        {/if}
+
+        <button class="fixed bottom-2 right-2 opacity-50" on:click={handleRefresh}>
+            <ArrowPathIcon class="h-6 w-6 {isSpinning ? 'animate-spin' : ''}" />
+        </button>
+    {:else}
+        <div class="hero">No tasks, try adding some</div>
+    {/if}
+{:catch error}
+    <div class="hero">Error loading Todoist data: {error.message}</div>
+{/await}
 
 {#if $todoistError}
     {@html error(`Error loading Todoist data: ${$todoistError}`) && ""}
