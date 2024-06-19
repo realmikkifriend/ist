98334eda407cd6f9158d126f723d8cc891ce6cbd
Mikki Friend <realmikkifriend@gmail.com>
2024-06-19 12:45:00 -0500

    refactor: heavily refactored to reduce redundancy

--- src/js/api.js
+++ src/js/api.js
@@ -5,37 +5,30 @@ import { filterAndSortDueTasks } from "./filter";
 import { success } from "./toasts";
 import { processTodoistData } from "./process";
 
+const API_URL = "https://api.todoist.com/sync/v9/sync";
+const CONTENT_TYPE = "application/x-www-form-urlencoded";
+
 export async function refreshData() {
     const RESOURCE_TYPES = ["items", "projects", "notes", "user"];
-    let error = null,
-        accessToken,
-        currentResources = {},
-        currentSyncToken;
-
-    $: accessToken = get(todoistAccessToken);
-
+    let error = null;
+    const accessToken = get(todoistAccessToken);
     if (!accessToken) {
-        error = "No access token found.";
-        todoistError.set(error);
-        return { resources: currentResources, error };
+        return setErrorState("No access token found.", {});
     }
 
-    $: currentSyncToken = get(syncToken);
-
+    const currentSyncToken = get(syncToken);
     try {
-        const data = await fetchTodoistData(RESOURCE_TYPES, currentSyncToken, accessToken);
+        const data = await executeAPICommand(
+            {
+                sync_token: currentSyncToken,
+                resource_types: JSON.stringify(RESOURCE_TYPES),
+            },
+            accessToken,
+        );
         syncToken.set(data.sync_token);
 
-        $: currentResources = get(todoistResources) || {};
-
+        let currentResources = get(todoistResources) || {};
         currentResources = processTodoistData(currentResources, data, RESOURCE_TYPES);
-    } catch (err) {
-        error = err.message;
-    }
-
-    if (error) {
-        todoistError.set(error);
-    } else {
         const timeZone = currentResources.user?.tz_info?.timezone || "UTC";
         todoistResources.set({
             ...currentResources,
@@ -45,98 +38,65 @@ export async function refreshData() {
                 timeZone,
             ),
         });
-
         success("Todoist data updated!");
+        return { resources: currentResources, error };
+    } catch (err) {
+        return setErrorState(err.message, {});
     }
+}
 
+export function setErrorState(error, currentResources) {
+    todoistError.set(error);
     return { resources: currentResources, error };
 }
 
-export async function fetchTodoistData(resourceTypes, syncToken, accessToken) {
-    const response = await fetch("https://api.todoist.com/sync/v9/sync", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
-            sync_token: syncToken,
-            resource_types: JSON.stringify(resourceTypes),
-        }),
-    });
-
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-}
-
 export async function markTaskDone(taskID, accessToken) {
-    const response = await fetch("https://api.todoist.com/sync/v9/sync", {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams({
+    return await executeAPICommand(
+        {
             commands: JSON.stringify([
-                {
-                    type: "item_close",
-                    uuid: uuidv4(),
-                    args: {
-                        id: taskID,
-                    },
-                },
+                { type: "item_close", uuid: uuidv4(), args: { id: taskID } },
             ]),
-        }),
-    });
-
-    if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+        },
+        accessToken,
+    );
 }
 
 export async function deferTasks(taskTimePairs, accessToken) {
-    const commands = taskTimePairs.map(([task, time]) => {
-        const taskNewDate =
-            task.due.all_day === 1
-                ? time.toFormat("yyyy-MM-dd")
-                : time.toFormat("yyyy-MM-dd'T'HH:mm:ss");
-
-        return {
-            type: "item_update",
-            uuid: uuidv4(),
-            args: {
-                id: task.id,
-                due: {
-                    date: taskNewDate,
-                    datetime: taskNewDate,
-                    string: task.due.string,
-                },
+    const commands = taskTimePairs.map(([task, time]) => ({
+        type: "item_update",
+        uuid: uuidv4(),
+        args: {
+            id: task.id,
+            due: {
+                date: formatTaskDate(task, time),
+                datetime: formatTaskDate(task, time),
+                string: task.due.string,
             },
-        };
-    });
+        },
+    }));
 
-    const response = await fetch("https://todoist.com/api/v9/sync", {
+    return await executeAPICommand({ commands: JSON.stringify(commands) }, accessToken);
+}
+
+async function executeAPICommand(params, accessToken) {
+    const response = await fetch(API_URL, {
         method: "POST",
         headers: {
             Authorization: `Bearer ${accessToken}`,
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Content-Type": CONTENT_TYPE,
         },
-        body: new URLSearchParams({
-            commands: JSON.stringify(commands),
-        }),
+        body: new URLSearchParams(params),
     });
 
     if (!response.ok) {
         throw new Error(`Error: ${response.status}`);
     }
 
-    const data = await response.json();
-    return data;
+    return response.json();
+}
+
+function formatTaskDate(task, time) {
+    return task.due.all_day === 1
+        ? time.toFormat("yyyy-MM-dd")
+        : time.toFormat("yyyy-MM-dd'T'HH:mm:ss");
 }
