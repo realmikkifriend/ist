6332ae30b8ffc067794359c6a6e064dbfc805159
Mikki Friend <realmikkifriend@gmail.com>
2024-06-20 16:45:48 -0500

    feat: items are rotated in Dynalist

--- src/html/task/dynalist/DynalistRotating.svelte
+++ src/html/task/dynalist/DynalistRotating.svelte
@@ -2,41 +2,56 @@
     import { onMount } from "svelte";
     import Markdown from "svelte-exmarkdown";
     import { ArrowUturnDownIcon } from "@krowten/svelte-heroicons";
-    import { generateDynalistComment } from "./dynalist";
+    import { generateDynalistComment, updateDynalist } from "./dynalist";
+    import { success } from "../../../js/toasts";
+
     export let content;
 
-    let checklistItems,
-        buttonElement,
-        currentIndex = 0;
+    let checklistItems, buttonElement;
 
     onMount(() => {
         checklistItems = content.children;
     });
 
-    function showNextItem() {
+    async function showNextItem() {
         buttonElement.classList.add("animate-ping");
 
-        setTimeout(() => {
-            const itemToMove = checklistItems.splice(0, 1)[0];
-            checklistItems = [...checklistItems, itemToMove];
+        const itemToMove = checklistItems.splice(0, 1)[0];
+        checklistItems = [...checklistItems, itemToMove];
+
+        try {
+            const changes = [
+                {
+                    action: "move",
+                    node_id: itemToMove.id,
+                    parent_id: content.id,
+                    index: -1,
+                },
+            ];
+
+            await updateDynalist(content.file_id, changes);
+
+            success("Sent to bottom of list in Dynalist!");
             buttonElement.classList.remove("animate-ping");
-        }, 350);
+        } catch (error) {
+            console.error("Failed to update Dynalist:", error);
+        }
     }
 </script>
 
-{#key checklistItems}
-    {#if checklistItems?.length > 0}
-        <div class="text-primary-content">
-            <button
-                bind:this={buttonElement}
-                class="float-left mr-2 mt-1 inline-block h-5 w-5 cursor-pointer rounded bg-primary p-1 pb-5 pr-5"
-                on:click={showNextItem}><ArrowUturnDownIcon class="h-4 w-4" /></button
-            >
+{#if checklistItems?.length > 0}
+    <div class="text-primary-content">
+        <button
+            bind:this={buttonElement}
+            class="float-left mr-2 mt-1 inline-block h-5 w-5 cursor-pointer rounded bg-primary p-1 pb-5 pr-5"
+            on:click={showNextItem}><ArrowUturnDownIcon class="h-4 w-4" /></button
+        >
+        {#key checklistItems}
             <Markdown
                 md={`${checklistItems[0].content}\n${generateDynalistComment(checklistItems[0])}`}
             />
-        </div>
-    {:else}
-        <span class="italic">No items in list!</span>
-    {/if}
-{/key}
+        {/key}
+    </div>
+{:else}
+    <span class="italic">No items in list!</span>
+{/if}
