5d619063c8985f4fc8264c6c981251f987c05c91
Mikki Friend <realmikkifriend@gmail.com>
2024-06-21 09:55:08 -0500

    feat: color elements when goal complete

--- src/html/task/dynalist/DynalistCount.svelte
+++ src/html/task/dynalist/DynalistCount.svelte
@@ -35,14 +35,19 @@
     <span class="w-full text-xl">{content.content}</span>
     <span class="flex w-full items-end justify-between">
         <span class="flex flex-nowrap items-baseline">
-            <span class="text-2xl">{current}</span>
+            <span class="text-2xl {current >= total ? 'text-green-500' : ''}">{current}</span>
             <span class="mx-1 text-lg">&#8725;{total}</span>
-            <small>{`${Math.round((current / total) * 100)}%`}</small>
+            <small class={current >= total ? "text-green-500" : ""}
+                >{`${Math.round((current / total) * 100)}%`}</small
+            >
         </span>
         <span class="flex flex-nowrap items-end">
             {#each options as option}
                 <button
-                    class="btn ml-1 h-8 min-h-8 bg-primary px-1 pb-1 pt-0 text-primary-content hover:bg-base-100 active:bg-primary"
+                    class="btn ml-1 h-8 min-h-8 px-1 pb-1 pt-0 text-primary-content hover:bg-base-100 active:bg-primary {current >=
+                    total
+                        ? 'bg-neutral'
+                        : 'bg-primary'}"
                     on:click={(event) => handleCount(option, event)}
                     >{option}
                 </button>
