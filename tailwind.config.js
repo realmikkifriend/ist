a9685274063a66de6c7445a7ccc5434790ad3c47
Mikki Friend <realmikkifriend@gmail.com>
2024-06-26 10:39:24 -0500

    fix: fixed outdated option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js,svelte,ts}"],
-    darkMode: "class",
+    darkMode: "selector",
     theme: {
         extend: {
             colors: {
