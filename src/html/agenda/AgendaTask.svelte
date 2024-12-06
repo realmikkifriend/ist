42281ea9f09439ffa1d4e8f05009b8502ad109b2
Mikki Friend <realmikkifriend@gmail.com>
2024-12-06 06:35:54 -0600

    feat: added summon button

--- src/html/agenda/AgendaTask.svelte
+++ src/html/agenda/AgendaTask.svelte
@@ -1,20 +1,27 @@
 <script>
     import { DateTime } from "luxon";
+    import { InboxArrowDownIcon } from "@krowten/svelte-heroicons";
     import { getPriorityClasses, colorClasses } from "../../js/classes";
 
     export let task, color;
+
+    function closeAgenda() {
+        window.location.hash = "";
+    }
 </script>
 
 <div
-    class={`agenda-task mx-1 w-full items-center overflow-hidden whitespace-nowrap rounded-md px-1 text-xs brightness-90 ${colorClasses[color] || "bg-gray-600"}`}
+    class={`agenda-task mx-1 flex w-full flex-row items-center overflow-hidden whitespace-nowrap rounded-md px-1 text-xs brightness-90 ${colorClasses[color] || "bg-gray-600"}`}
 >
-    <span
-        class="priority-element relative -left-1.5 mt-0 inline-block h-fit min-h-2.5 min-w-4 rounded-md px-1.5 text-xs font-bold {getPriorityClasses(
+    <button
+        on:click={closeAgenda}
+        class="priority-element relative -left-1.5 mt-0 flex h-fit min-h-2.5 min-w-10 flex-shrink-0 flex-row items-center justify-center rounded-md pl-2 pr-1 text-xs font-bold {getPriorityClasses(
             task.priority,
         )}"
     >
         {#if task.due.date.includes("T")}
             {DateTime.fromISO(task.due.date).toFormat("h:mm")}
         {/if}
-    </span>{task.content}
+        <InboxArrowDownIcon class="h-3 min-h-3 w-4 min-w-4" />
+    </button>{task.content}
 </div>
