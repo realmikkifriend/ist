0bd39c4997aee4eeb597924c2af4d44cce7bd3bc
Mikki Friend <realmikkifriend@gmail.com>
2025-07-18 12:57:50 -0500

    feat: add button to summon reverse task list

--- src/html/agenda/AgendaHeader.svelte
+++ src/html/agenda/AgendaHeader.svelte
@@ -1,10 +1,14 @@
 <script>
-    import { XCircleIcon, CalendarIcon } from "@krowten/svelte-heroicons";
+    import { XCircleIcon, CalendarIcon, BarsArrowUpIcon } from "@krowten/svelte-heroicons";
 
     export let title, tasks, tasksWithNoTime, todayTasks, headerGradientColor;
 
     function switchView() {
         window.location.hash = window.location.hash === "#today" ? "#tomorrow" : "#today";
+    }
+
+    function viewReverseTaskList() {
+        // TODO: Implement functionality
     }
 
     function closeAgenda() {
@@ -15,11 +19,17 @@
 <div class="flex items-center justify-between pb-2 pl-16">
     <button
         on:click={switchView}
-        class="rounded-full p-2 transition-colors duration-200 hover:bg-blue-800"
+        class="rounded-full p-1 transition-colors duration-200 hover:bg-blue-800"
     >
         <CalendarIcon class="h-5 w-6" />
     </button>
-    <div class="flex flex-col items-center">
+    <button
+        on:click={viewReverseTaskList}
+        class="rounded-full p-1 transition-colors duration-200 hover:bg-blue-800"
+    >
+        <BarsArrowUpIcon class="h-5 w-6" />
+    </button>
+    <div class="flex flex-grow flex-col items-center">
         <h1 class="flex-1 text-center">{title}</h1>
         <h2 class="rounded-badge px-3 py-0.5 text-center {headerGradientColor}">
             {#if todayTasks.length > 0 && window.location.hash === "#tomorrow"}
@@ -35,7 +45,7 @@
     </div>
     <button
         on:click={closeAgenda}
-        class="rounded-full p-2 transition-colors duration-200 hover:bg-red-700"
+        class="rounded-full p-1 transition-colors duration-200 hover:bg-red-700"
     >
         <XCircleIcon class="h-5 w-6" />
     </button>
