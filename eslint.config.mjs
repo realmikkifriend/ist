44c70490cca78212758160e3388694703679daaf
Mikki Friend <realmikkifriend@gmail.com>
2025-07-18 23:51:57 -0500

    build: added eslint and functional programming rules

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import functional from "eslint-plugin-functional";
+
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+    { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+    { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+    functional.configs.externalVanillaRecommended,
+    functional.configs.recommended,
+    functional.configs.stylistic,
+    functional.configs.disableTypeChecked,
+    {
+        rules: {},
+    },
+]);
