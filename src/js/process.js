c5ab304d93d89ae48cf348a3b6701c0709f00562
Mikki Friend <realmikkifriend@gmail.com>
2025-07-19 00:40:13 -0500

    refactor: change lists of variables to strings, remove for loop

--- src/js/process.js
+++ src/js/process.js
@@ -21,109 +21,120 @@ function handleOverdueTasks(tasks) {
     }
 }
 
+function cleanDataArray(dataArray, propsToRemove, renameMap = {}) {
+    return dataArray.map((item) =>
+        propsToRemove.reduce(
+            (acc, prop) => {
+                if (renameMap[prop] && acc[prop] !== undefined) {
+                    acc[renameMap[prop]] = acc[prop];
+                }
+                delete acc[prop];
+                return acc;
+            },
+            { ...item },
+        ),
+    );
+}
+
 export function cleanTodoistData(data) {
     if (data.tasks) {
         handleOverdueTasks(data.tasks);
+        const taskPropsToRemove = [
+            "userId",
+            "sectionId",
+            "parentId",
+            "addedByUid",
+            "assignedByUid",
+            "responsibleUid",
+            "deadline",
+            "checked",
+            "description",
+            "isDeleted",
+            "addedAt",
+            "completedAt",
+            "updatedAt",
+            "noteCount",
+            "isCollapsed",
+            "dayOrder",
+            "projectId",
+        ];
+        const taskRenameMap = {
+            projectId: "contextId",
+        };
+        data.tasks = cleanDataArray(data.tasks, taskPropsToRemove, taskRenameMap);
+    }
 
-        data.tasks = data.tasks.map((task) => {
-            const {
-                userId,
-                sectionId,
-                parentId,
-                addedByUid,
-                assignedByUid,
-                responsibleUid,
-                deadline,
-                checked,
-                description,
-                isDeleted,
-                addedAt,
-                completedAt,
-                updatedAt,
-                noteCount,
-                isCollapsed,
-                dayOrder,
-                projectId,
-                ...rest
-            } = task;
-            if (projectId !== undefined) {
-                rest.contextId = projectId;
-            }
-            return rest;
-        });
-    }
     if (data.contexts) {
-        data.contexts = data.contexts.map((context) => {
-            const {
-                canAssignTasks,
-                createdAt,
-                isArchived,
-                isDeleted,
-                isFavorite,
-                isFrozen,
-                updatedAt,
-                viewStyle,
-                defaultOrder,
-                description,
-                isCollapsed,
-                isShared,
-                parentId,
-                ...rest
-            } = context;
-            return rest;
-        });
+        const contextPropsToRemove = [
+            "canAssignTasks",
+            "createdAt",
+            "isArchived",
+            "isDeleted",
+            "isFavorite",
+            "isFrozen",
+            "updatedAt",
+            "viewStyle",
+            "defaultOrder",
+            "description",
+            "isCollapsed",
+            "isShared",
+            "parentId",
+        ];
+        data.contexts = cleanDataArray(data.contexts, contextPropsToRemove);
     }
+
     if (data.user) {
-        const {
-            activated_user,
-            auto_reminder,
-            avatar_big,
-            avatar_medium,
-            avatar_s640,
-            avatar_small,
-            business_account_id,
-            completed_count,
-            date_format,
-            deleted_at,
-            feature_identifier,
-            features,
-            has_magic_number,
-            has_password,
-            has_started_a_trial,
-            image_id,
-            id,
-            inbox_project_id,
-            is_celebrations_enabled,
-            is_deleted,
-            is_premium,
-            joinable_workspace,
-            joined_at,
-            karma,
-            karma_trend,
-            lang,
-            mfa_enabled,
-            onboarding_level,
-            onboarding_persona,
-            onboarding_role,
-            onboarding_started,
-            onboarding_team_mode,
-            onboarding_use_cases,
-            premium_status,
-            premium_until,
-            shard_id,
-            share_limit,
-            sort_order,
-            start_day,
-            start_page,
-            theme_id,
-            time_format,
-            unique_prefix,
-            verification_status,
-            websocket_url,
-            weekend_start_day,
-            ...rest
-        } = data.user;
-        data.user = rest;
+        const userPropsToRemove = [
+            "activated_user",
+            "auto_reminder",
+            "avatar_big",
+            "avatar_medium",
+            "avatar_s640",
+            "avatar_small",
+            "business_account_id",
+            "completed_count",
+            "date_format",
+            "deleted_at",
+            "feature_identifier",
+            "features",
+            "has_magic_number",
+            "has_password",
+            "has_started_a_trial",
+            "image_id",
+            "id",
+            "inbox_project_id",
+            "is_celebrations_enabled",
+            "is_deleted",
+            "is_premium",
+            "joinable_workspace",
+            "joined_at",
+            "karma",
+            "karma_trend",
+            "lang",
+            "mfa_enabled",
+            "onboarding_level",
+            "onboarding_persona",
+            "onboarding_role",
+            "onboarding_started",
+            "onboarding_team_mode",
+            "onboarding_use_cases",
+            "premium_status",
+            "premium_until",
+            "shard_id",
+            "share_limit",
+            "sort_order",
+            "start_day",
+            "start_page",
+            "theme_id",
+            "time_format",
+            "unique_prefix",
+            "verification_status",
+            "websocket_url",
+            "weekend_start_day",
+        ];
+
+        data.user = cleanDataArray([data.user], userPropsToRemove)[0];
     }
+
     return data;
 }
