52dc73e79d927fb8be0db8f90b460e4ed23383a7
Mikki Friend <realmikkifriend@gmail.com>
2025-07-19 09:19:17 -0500

    refactor: moved button config to new helper file

--- src/html/defer/deferButtons.js
+++ src/html/defer/deferButtons.js
@@ -1,45 +1,6 @@
+import { buttonConfig } from "./deferButtonsTime.js";
+
 const createButtons = () => {
-    const buttonConfig = {
-        tomorrow: {
-            text: "tomorrow",
-            ms: 0,
-            styling: "basis-full",
-            stylingButton: "h-6 bg-blue-900",
-        },
-
-        minutes: [
-            { value: 1, height: "h-5" },
-            { value: 3, height: "h-5" },
-            { value: 10, height: "h-6" },
-            { value: 15, height: "h-6" },
-            { value: 30, height: "h-7" },
-            { value: 45, height: "h-7" },
-        ],
-    };
-
-    const baseStyling = "basis-[22.75%]";
-    const largeStyling = "basis-[48.5%]";
-
-    const hoursValues = [
-        { value: 1, text: "1 hour", styling: largeStyling },
-        { value: 1.5, text: "90 minutes", styling: largeStyling },
-        { value: 2 },
-        { value: 3 },
-        { value: 4 },
-        { value: 6 },
-        { value: 8 },
-        { value: 12 },
-        { value: 18 },
-        { value: 24 },
-    ];
-
-    buttonConfig.hours = hoursValues.map((item) => ({
-        value: item.value,
-        text: item.text || undefined,
-        height: "h-7",
-        styling: item.styling || baseStyling,
-    }));
-
     const buttons = [];
 
     buttons.push(buttonConfig.tomorrow);
