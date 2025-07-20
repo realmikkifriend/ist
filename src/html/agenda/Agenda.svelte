f457f9bea397b776fc480c373f5fefacede289c0
Mikki Friend <realmikkifriend@gmail.com>
2025-07-20 17:42:52 -0500

    refactor: added keys to each, reduced variable mutation

--- src/html/agenda/Agenda.svelte
+++ src/html/agenda/Agenda.svelte
@@ -9,22 +9,25 @@
     import AgendaTask from "./AgendaTask.svelte";
     import { getGradientColor } from "../../js/classes.js";
 
-    let title = "";
     let tasks,
         tasksForDate,
         tasksWithNoTime,
         todayTasks = [];
-    let hourSlots = Array.from({ length: 18 }, (_, i) => i + 6);
+    const hourSlots = Array.from({ length: 18 }, (_, i) => i + 6);
     let displayHours = {};
     let headerGradientColor;
     let now;
 
     $: $todoistData, updatePage();
 
+    const getTitle = () => {
+        return window.location.hash.replace("#", "").replace(/^./, (c) => c.toUpperCase());
+    };
+
     const updatePage = () => {
         now = DateTime.now();
 
-        title = window.location.hash.replace("#", "").replace(/^./, (c) => c.toUpperCase());
+        const currentTitle = getTitle();
 
         const targetDate =
             window.location.hash === "#today"
@@ -51,7 +54,7 @@
             tasksWithNoTime = filterAndSortTasks(tasksWithNoTime, $todoistData.contexts);
         }
 
-        if (title === "Today") {
+        if (currentTitle === "Today") {
             displayHours = {};
             hourSlots.forEach((hour) => {
                 const hourTasks = tasks.filter(
@@ -93,38 +96,40 @@
     });
 </script>
 
-<div class="mr-4 mt-[-2rem] max-w-lg sm:mx-auto sm:max-w-96" id="agenda">
-    <AgendaHeader
-        {title}
-        {tasks}
-        {tasksWithNoTime}
-        {todayTasks}
-        {tasksForDate}
-        {headerGradientColor}
-    />
+{#key tasks}
+    <div class="mr-4 mt-[-2rem] max-w-lg sm:mx-auto sm:max-w-96" id="agenda">
+        <AgendaHeader
+            title={getTitle()}
+            {tasks}
+            {tasksWithNoTime}
+            {todayTasks}
+            {tasksForDate}
+            {headerGradientColor}
+        />
 
-    {#if tasksWithNoTime.length > 0}
-        <div class="mb-4 flex w-full flex-col items-center pl-[4.5rem] pr-2">
-            {#each tasksWithNoTime as task}
-                <AgendaTask {task} color={getTaskColor(task.contextId)} />
-            {/each}
+        {#if tasksWithNoTime.length > 0}
+            <div class="mb-4 flex w-full flex-col items-center pl-[4.5rem] pr-2">
+                {#each tasksWithNoTime as task (task.id)}
+                    <AgendaTask {task} color={getTaskColor(task.contextId)} />
+                {/each}
+            </div>
+        {/if}
+
+        <div class="w-[99%] overflow-hidden pr-1">
+            {#key (now.hour, tasks)}
+                {#each hourSlots as hour (hour)}
+                    {#if displayHours[hour]}
+                        <AgendaHour
+                            title={getTitle()}
+                            {hour}
+                            {now}
+                            tasks={tasks.filter(
+                                (task) => DateTime.fromISO(task.due.date).hour === hour,
+                            )}
+                        />
+                    {/if}
+                {/each}
+            {/key}
         </div>
-    {/if}
-
-    <div class="w-[99%] overflow-hidden pr-1">
-        {#key (now.hour, tasks)}
-            {#each hourSlots as hour}
-                {#if displayHours[hour]}
-                    <AgendaHour
-                        {title}
-                        {hour}
-                        {now}
-                        tasks={tasks.filter(
-                            (task) => DateTime.fromISO(task.due.date).hour === hour,
-                        )}
-                    />
-                {/if}
-            {/each}
-        {/key}
     </div>
-</div>
+{/key}
