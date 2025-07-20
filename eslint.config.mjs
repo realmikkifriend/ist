0da0a07d6ab37e97d8a56e2d79a350b3f1601142
Mikki Friend <realmikkifriend@gmail.com>
2025-07-20 16:30:46 -0500

    build: cleaned up Svelte linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,14 +8,11 @@ export default defineConfig([
     { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
     { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
     { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-    // Svelte configuration
-    ...svelte.configs["flat/recommended"],
-    {
-        files: ["**/*.svelte"],
-        languageOptions: {
-            globals: globals.browser,
-        },
-    },
+    // -----------------------------------
+    // `     eslint-plugin-svelte
+    ...svelte.configs.recommended,
+    // -----------------------------------
+    // `     eslint-plugin-functional
     // functional.configs.externalVanillaRecommended,
     // functional.configs.recommended,
     // functional.configs.strict,
