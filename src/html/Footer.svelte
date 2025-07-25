3258f51a15f1cff9b34f279d3ff76bb0a93683e0
Mikki Friend <realmikkifriend@gmail.com>
2025-07-25 17:39:45 -0500

    fix: added script section to be recognized as TypeScript

--- src/html/Footer.svelte
+++ src/html/Footer.svelte
@@ -1,3 +1,5 @@
+<script lang="ts"></script>
+
 <footer class="mb-2 px-0 text-center opacity-50">
     <p class="text-sm italic">
         Ist is not created by, affiliated with, or supported by Doist. None of your data is stored.
