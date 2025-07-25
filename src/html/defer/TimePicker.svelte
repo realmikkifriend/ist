17f3e9578b840f51f96cd38c4bb4f53d5b715885
Mikki Friend <realmikkifriend@gmail.com>
2025-07-25 15:16:23 -0500

    refactor: converted time picker component to TypeScript

--- src/html/defer/TimePicker.svelte
+++ src/html/defer/TimePicker.svelte
@@ -1,53 +1,58 @@
-<script>
+<script lang="ts">
     import { createEventDispatcher, onDestroy } from "svelte";
     import { getPriorityClasses } from "../../js/classes";
     import { updateMilliseconds } from "./defer";
+    import type { Task } from "../../../types/todoist";
+    // import type { TimeButtonConfig } from "../../../types/defer";
 
-    export let task, tasks;
+    export let task: Task;
+    export let tasks: Task[];
 
-    $: buttons = updateMilliseconds(task, tasks);
-
-    const triggerButtonUpdate = () => {
+    const triggerButtonUpdate = (): void => {
         tasks = [...tasks];
     };
 
     const interval = setInterval(triggerButtonUpdate, 30000);
 
-    onDestroy(() => {
+    onDestroy((): void => {
         clearInterval(interval);
     });
 
-    const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher<{ defer: { rawTime: number } }>();
 
-    const handleDefer = (rawTime) => {
+    const handleDefer = (rawTime: number): void => {
         dispatch("defer", { rawTime });
     };
 </script>
 
 <div class="mt-2 flex w-72 flex-row flex-wrap gap-x-2 gap-y-1">
-    {#each buttons as button (button.ms)}
-        <div class={button.styling}>
-            <button
-                class="btn min-h-4 w-full rounded-md px-1 hover:bg-secondary {button.stylingButton}"
-                on:click={() => handleDefer(button.ms)}
-                >{button.text}
-            </button>
-            {#if button.time}
-                <div class="flex max-h-4 w-full justify-between overflow-hidden text-xs opacity-65">
-                    <span
-                        class="w-fit overflow-hidden text-left text-secondary"
-                        class:italic={button.time && button.time.startsWith("*")}
-                        >{button.time && button.time.startsWith("*")
-                            ? button.time.slice(1)
-                            : button.time}</span
+    {#each updateMilliseconds(task, tasks) as button (button.ms)}
+        {#if button.ms !== undefined}
+            <div class={button.styling}>
+                <button
+                    class={"btn min-h-4 w-full rounded-md px-1 hover:bg-secondary " +
+                        button.stylingButton}
+                    on:click={() => handleDefer(Number(button.ms))}>{button.text}</button
+                >
+                {#if button.time}
+                    <div
+                        class="flex max-h-4 w-full justify-between overflow-hidden text-xs opacity-65"
                     >
-                    <span
-                        class="badge badge-xs mt-0.5 w-fit overflow-hidden text-right font-bold {getPriorityClasses(
-                            button.priority,
-                        )}">{button.count}</span
-                    >
-                </div>
-            {/if}
-        </div>
+                        <span
+                            class="w-fit overflow-hidden text-left text-secondary"
+                            class:italic={button.time && button.time.startsWith("*")}
+                            >{button.time && button.time.startsWith("*")
+                                ? button.time.slice(1)
+                                : button.time}</span
+                        >
+                        <span
+                            class="badge badge-xs mt-0.5 w-fit overflow-hidden text-right font-bold {getPriorityClasses(
+                                button.priority ?? 1,
+                            )}">{button.count}</span
+                        >
+                    </div>
+                {/if}
+            </div>
+        {/if}
     {/each}
 </div>
