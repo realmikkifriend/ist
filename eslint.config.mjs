2643c72ae0f8a80f4ed5899a7f1d8530646400d5
Mikki Friend <realmikkifriend@gmail.com>
2025-08-04 17:26:19 -0500

    build: added util import boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -80,6 +80,28 @@ export default defineConfig([
         },
     },
     // -----------------------------------
+    // `     Architectural Boundaries
+    {
+        files: ["src/utils/**/*.ts"],
+        rules: {
+            "no-restricted-imports": [
+                "error",
+                {
+                    patterns: [
+                        {
+                            group: ["../services/*"],
+                            message: "Utilities can't use services.",
+                        },
+                        {
+                            group: ["../stores/*"],
+                            message: "Utilities can't access stores.",
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    // -----------------------------------
     // `     Svelte Configuration
     ...svelte.configs.recommended,
     {
