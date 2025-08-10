67a32c138d46b41926572a407e07432f2d60ea43
Mikki Friend <realmikkifriend@gmail.com>
2025-08-09 20:18:34 -0500

    feat: added hint to agenda shortcut

--- src/html/NoTasks.svelte
+++ src/html/NoTasks.svelte
@@ -3,15 +3,16 @@
     import { openAgenda } from "../services/agendaService";
 
     const buttonClasses: string =
-        "border-2 space-x-1 border-white rounded-md p-2 flex flex-row items-center";
+        "relative border-2 space-x-1 border-white rounded-md p-2 flex flex-row items-center";
 </script>
 
 <div class="hero flex flex-col">
     <span>No due tasks...</span>
 
-    <div class="mt-3 flex flex-row space-x-4">
+    <div class="y mt-3 flex flex-row space-x-4">
         <button class={buttonClasses} onclick={() => openAgenda("today")} type="button">
             <Icon class="h-4 w-5" src={Calendar} /> <span>Today</span>
+            <kbd>a</kbd>
         </button>
         <button class={buttonClasses} onclick={() => openAgenda("tomorrow")} type="button">
             <Icon class="h-4 w-5" src={Calendar} /> <span>Tomorrow</span>
