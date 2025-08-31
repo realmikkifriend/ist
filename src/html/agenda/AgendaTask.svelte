3156d7887f3b6db83747d5c2b05febca75e31df3
Mikki Friend <realmikkifriend@gmail.com>
2025-08-31 08:33:50 -0500

    style: tweaked icon

--- src/html/agenda/AgendaTask.svelte
+++ src/html/agenda/AgendaTask.svelte
@@ -49,9 +49,9 @@
             class="relative -left-0.25 mr-0.75 rounded-full bg-green-200 outline-3 outline-green-200"
             title="task is never marked done"
         >
-            <Icon class="relative h-2 w-2 stroke-5 text-green-700" src={Check} />
+            <Icon class="relative top-[0.02em] h-2 w-2 stroke-5 text-green-700" src={Check} />
             <Icon
-                class="absolute -top-[0.27em] -left-[0.24em] h-[1.2em] w-[1.2em] stroke-3 text-red-500"
+                class="absolute -top-[0.27em] -left-[0.24em] h-[1.2em] w-[1.15em] stroke-3 text-red-500"
                 src={NoSymbol}
             />
         </div>
